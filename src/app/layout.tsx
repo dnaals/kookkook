37a98a38a9229e3c17906b65241397c47802c3cc
dnaals <swm8793@naver.com>
2024-02-20 13:15:01 +0900

    asd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +14,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+
+
+        {children}
+      </body>
     </html>
+
   );
 }
