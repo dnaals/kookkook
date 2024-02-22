51516989da196ff7a90b405c7f51b7234d1104d6
simhyesoo <sgptn00@gmail.com>
2024-02-22 16:49:06 +0900

    뭔가이상해

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import "@/components/style/login.scss";
+import Link from "next/link";
 
 export default function Home() {
   return (
 
     <div>
       이게 로그인 페이지<br />
-
+      <Link href='/home'>비회원로그인</Link>
     </div>
   );
 }
