72c34d4d9d1ce6721a8ae5ead5592b797efceece
SSkkky <worte5633@gmail.com>
2024-02-23 16:05:56 +0900

    6

--- src/components/UIUX/RecipeSq.tsx
+++ src/components/UIUX/RecipeSq.tsx
@@ -1,1 +1,49 @@
 //레시피 정사각형 모양
+//헤더메뉴
+'use client';
+
+import { useEffect } from "react";
+import { useStore } from '@/components/recipe_store/all_store';
+import { useStore2 } from '@/components/recipe_store/my_store';
+import React from 'react';
+import { useRouter } from "next/navigation";
+
+
+function RecipeSq() {
+
+        let { data, dataCrl } = useStore();
+        let { data2, dataCrl2 } = useStore2();
+    
+        useEffect(() => {
+            dataCrl('all', '');
+            dataCrl2('all', '');
+        }, [])
+        if (!data.length) return <>sadsadsa...</>
+
+const router = useRouter();
+
+        const link = (a:any)=>{
+            let aa = data.filter(obj=> a==obj.name)
+            let url = aa[0].seq;
+            router.push(`/home/seq=${url}`)
+        }
+    return (
+        <>
+            <button onClick={() => { dataCrl('카테고리', '반찬') }}>반찬</button>
+            <button onClick={() => { dataCrl('카테고리', '밥') }}>밥</button>
+            <button onClick={() => { dataCrl('카테고리', '국&찌개') }}>국&찌개</button>
+            <button onClick={() => { dataCrl('카테고리', '일품') }}>일품</button>
+            <button onClick={() => { dataCrl('카테고리', '후식') }}>후식</button><br />
+            <button onClick={() => { dataCrl2('all', '') }}>구글계정 모두</button>
+            {data.map((obj,k)=>(
+                <div key={k}>
+                    
+             <p onClick={()=>{link(obj.name)}}>{obj.name}</p>   
+             </div>
+            ))}
+        </>
+        
+    );
+}
+
+export default RecipeSq;
